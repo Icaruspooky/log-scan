7e169e253d37025b93c6001d880e1f2abbd14ffb
Icarus <icaroaj@hotmail.com>
2020-04-24 10:08:12 +0100

    Thread separation

--- src/log_scan/core.clj
+++ src/log_scan/core.clj
@@ -6,26 +6,29 @@
 (defn -main
   [& args]
   (def file (slurp "/home/icarus/dev/test.log"))
-  (def renders (str/split file #"Executing request startRendering"))
   (def lines (str/split-lines file))
   (def thread-names (distinct (for [line lines] (StringUtils/substringBefore (StringUtils/substringAfter line "[") "]"))))
   (def threads (for [thread thread-names]
                       (filter #(str/includes? % thread) lines)))
   (def xml-report
     (xml/element :report {}
-                 (for [render renders]
+                 (for [thread threads]
                    (xml/element :rendering {}
-                                (xml/element :document {}(StringUtils/substringBefore (StringUtils/substringAfter render "{arguments=[") ","))
-                                (xml/element :page {} (StringUtils/substringBefore (StringUtils/substringAfter render ", ") "]"))
-                                (xml/element :uid {} (StringUtils/substringBefore (StringUtils/substringAfter render "Service startRendering returned ") " "))
-                                (for [aux renders]
-                                  (if (not= aux render)
-                                  (xml/element :start {} "2010-10-06 09:03:05,873")))
-                                (for [aux renders]
-                                  (if (not= aux render)
-                                  (xml/element :get {} "2010-10-06 09:03:05,873")))))
+                                (for [line thread]
+                                  (if (str/includes? line "Executing request startRendering")
+                                    (xml/element :document {}(StringUtils/substringBefore (StringUtils/substringAfter line "{arguments=[") ","))))
+                                (for [line thread]
+                                  (if (str/includes? line "Executing request startRendering")
+                                    (xml/element :page {} (StringUtils/substringBefore (StringUtils/substringAfter line ", ") "]"))))
+                                (for [line thread]
+                                  (if (str/includes? line "Service startRendering returned")
+                                    (xml/element :uid {} (StringUtils/substringBefore (StringUtils/substringAfter line "Service startRendering returned ") " "))))
+                                (for [aux threads]
+                                  (if (not= aux thread) (xml/element :start {} "2010-10-06 09:03:05,873")))
+                                (for [aux threads]
+                                  (if (not= aux thread) (xml/element :get {} "2010-10-06 09:03:05,873")))))
                    (xml/element :summary {}
-                                (xml/element :count {} (count renders))
-                                (xml/element :duplicates {} (count renders))
-                                (xml/element :unnecessary {} (count renders)))))
+                                (xml/element :count {} (count threads))
+                                (xml/element :duplicates {} (count threads))
+                                (xml/element :unnecessary {} (count threads)))))
   (println (xml/emit-str xml-report)))
