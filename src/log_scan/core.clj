5d03516198930e08fd31b0ddefd83d68e865c3da
Icarus <icaroaj@hotmail.com>
2020-04-26 23:32:05 +0100

    Implemented unecessary count

--- src/log_scan/core.clj
+++ src/log_scan/core.clj
@@ -1,6 +1,7 @@
 (ns log-scan.core
   (:require [clojure.string :as str])
-  (:require [clojure.data.xml :as xml])
+  (:require [clojure.data.xml :as xml]
+            [clojure.set :as set])
   (:import [org.apache.commons.lang3 StringUtils]))
 
 (defn -main
@@ -10,7 +11,10 @@
   (def file-path (read-line))
   (def file (slurp file-path))
   (def lines (str/split-lines file))
-  (def uids (frequencies (for [line lines]  (StringUtils/substringBefore (StringUtils/substringAfter line "Service startRendering returned ") " "))))
+  (def uids (distinct (for [line lines]  (StringUtils/substringBefore (StringUtils/substringAfter line "Service startRendering returned ") " "))))
+  (def gets (distinct (for [line lines]  (StringUtils/substringBefore (StringUtils/substringAfter line "Executing request getRendering with arguments [") "]"))))
+  (def gets-freq (frequencies (set/union uids gets)))
+  (def uids-freq (frequencies (for [line lines]  (StringUtils/substringBefore (StringUtils/substringAfter line "Service startRendering returned ") " "))))
   (def thread-names (distinct (for [line lines] (StringUtils/substringBefore (StringUtils/substringAfter line "[") "]"))))
   (def threads (for [thread thread-names] (filter #(str/includes? % (str/join ["[" thread "]"])) lines)))
   (def renders (for [thread threads] (partition-by #(str/includes? % "Service startRendering returned") thread)))
@@ -22,11 +26,18 @@
                    (for [render thread]
                      (if (str/includes? (str/join render) "Executing request startRendering")
                        (xml/element :rendering {}
-                                    (xml/element :document {}(StringUtils/substringBefore (StringUtils/substringAfter (StringUtils/substringAfter (str/join render) "Executing request startRendering") "with arguments [") ","))
-
-                                    (xml/element :page {} (StringUtils/substringBefore (StringUtils/substringAfter (StringUtils/substringAfter (str/join render) "Executing request startRendering")", ") "] "))
-
-                                    (xml/element :uid {} (StringUtils/substringBefore (StringUtils/substringAfter (StringUtils/substringAfter (str/join render) "Executing request startRendering") "{ RenderingCommand - uid: ") " }"))
+                                    (xml/element :document {}(StringUtils/substringBefore
+                                                               (StringUtils/substringAfter
+                                                                 (StringUtils/substringAfter
+                                                                   (str/join render) "Executing request startRendering") "with arguments [") ","))
+                                    (xml/element :page {} (StringUtils/substringBefore
+                                                            (StringUtils/substringAfter
+                                                              (StringUtils/substringAfter
+                                                                (str/join render) "Executing request startRendering")", ") "] "))
+                                    (xml/element :uid {} (StringUtils/substringBefore
+                                                           (StringUtils/substringAfter
+                                                             (StringUtils/substringAfter
+                                                               (str/join render) "Executing request startRendering") "{ RenderingCommand - uid: ") " }"))
                                     ;(for [aux threads]
                                     ;  (if (not= aux thread) (xml/element :start {} "2010-10-06 09:03:05,873")))
                                     ;(for [aux threads]
@@ -34,7 +45,7 @@
                                     ) "")))
                  (xml/element :summary {}
                               (xml/element :count {} (reduce +(for [line lines] (if (str/includes? line "Executing request startRendering") 1 0))))
-                              (xml/element :duplicates {} (reduce + (for [uid uids] (if (> (get uid 1) 1) 1 0))))
-                              (xml/element :unnecessary {} (count threads)))))
+                              (xml/element :duplicates {} (reduce + (for [uid uids-freq] (if (> (get uid 1) 1) 1 0))))
+                              (xml/element :unnecessary {} (reduce + (for [gets gets-freq] (if (= (get gets 1) 1) 1 0)))))))
   (println (xml/emit-str xml-report)))
 
